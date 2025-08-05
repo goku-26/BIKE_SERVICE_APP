0f5e464a7da0930a7c153770796950e9be4705a6
goku-26 <gokulp2608@gmail.com>
2025-08-05 09:23:06 +0530

    Feat:Added some changes in the Bike List

--- backend/controllers/bikeController.js
+++ backend/controllers/bikeController.js
@@ -1,12 +1,29 @@
-const brands = ['Honda', 'Yamaha', 'Suzuki', 'TVS', 'Royal Enfield'];
+const brands = [
+  'Honda',
+  'Yamaha',
+  'Suzuki',
+  'TVS',
+  'Royal Enfield',
+  'KTM',
+  'Hero',
+  'Bajaj',
+  'BMW',
+  'Ducati'
+];
 
 const bikeModels = {
-  Honda: ['Shine', 'Unicorn', 'SP 125'],
-  Yamaha: ['R15', 'FZ', 'MT-15'],
-  Suzuki: ['Access', 'Gixxer'],
-  TVS: ['Apache', 'Jupiter'],
-  'Royal Enfield': ['Classic 350', 'Bullet 350']
+  Honda: ['Shine', 'Unicorn', 'SP 125', 'Hornet 2.0', 'CB 350'],
+  Yamaha: ['R15', 'FZ', 'MT-15', 'Fascino', 'Ray ZR'],
+  Suzuki: ['Access', 'Gixxer', 'Burgman Street', 'Intruder'],
+  TVS: ['Apache', 'Jupiter', 'Ntorq', 'Radeon'],
+  Royal_Enfield: ['Classic 350', 'Bullet 350', 'Hunter 350', 'Meteor 350','Interceptor 650','Continental GT 650'],
+  KTM: ['Duke 200', 'RC 200', 'Adventure 390', 'Duke 390','RC 390'],
+  Hero: ['Splendor Plus', 'Glamour', 'Xtreme 160R', 'HF Deluxe'],
+  Bajaj: ['Pulsar 150', 'Pulsar NS200', 'Avenger 220', 'Dominar 400'],
+  BMW: ['G 310 R', 'G 310 GS', 'R 1250 GS'],
+  Ducati: ['Monster', 'Panigale V4', 'Scrambler', 'Multistrada']
 };
+
 
 exports.getBrands = (req, res) => {
   res.json(brands);
